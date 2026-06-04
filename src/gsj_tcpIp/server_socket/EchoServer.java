4da6baea4efddee10c1f187d90e3351c87a03c79
kyusang4657 <kyusang4657@gmail.com>
2026-06-04 14:41:55 +0900

    Create EchoServer

--- src/gsj_tcpIp/server_socket/EchoServer.java
+++ src/gsj_tcpIp/server_socket/EchoServer.java
@@ -1,4 +1,34 @@
 package gsj_tcpIp.server_socket;
 
+import java.io.*;
+import java.net.*;
+
 public class EchoServer {
+    public static void main(String[] args){
+        String theLine;
+
+        try(ServerSocket theServer = new ServerSocket(7)){
+            while(true){
+                try(Socket theSocket = theServer.accept();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(theSocket.getInputStream()));
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(theSocket.getOutputStream()));){
+
+                    while((theLine = reader.readLine()) != null){
+                        System.out.println(theLine);
+
+                        writer.write(theLine);
+                        writer.newLine();
+                        writer.flush();
+                    }
+
+                }catch(IOException ioe){
+                    System.err.println(ioe);
+                }
+            }
+        }catch(IOException ioe){
+            System.err.println(ioe);
+        }
+    }
 }
